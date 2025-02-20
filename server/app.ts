f680f195d6513f665547a8705b90033d8d2127e3
Louis Dierickx <dierickx.louis01@gmail.com>
2025-02-20 14:30:09 +0100

    1

--- server/app.ts
+++ server/app.ts
@@ -1,21 +1,29 @@
-import express, { Application, Request, Response } from "express";
+import express, { Application } from "express";
 import path from "path";
+import expressLayouts from "express-ejs-layouts";
 import routes from "./routes";
-
 
 const app: Application = express();
 const PORT: number = 3000;
 
-app.get("/", (req: Request, res: Response): void => {
-  res.sendFile(path.join(__dirname, "/views/index.html"));
-});
+// EJS als template-engine instellen
+app.set("view engine", "ejs");
+app.set("views", "./server/views");
 
+// Middleware voor layouts
+app.use(expressLayouts);
+app.set("layout", "layouts/main");
+
+// Middleware om statische bestanden te serveren
 app.use(express.static(path.join(__dirname, "/public")));
 
+// Middleware om formulierdata te verwerken
+app.use(express.urlencoded({ extended: true }));
 
+// Routes gebruiken
+app.use("/", routes);
 
-app.listen(PORT, () => {
+// Server starten
+app.listen(PORT, (): void => {
   console.log(`Server draait op http://localhost:${PORT}`);
 });
-
-app.use("/", routes);
